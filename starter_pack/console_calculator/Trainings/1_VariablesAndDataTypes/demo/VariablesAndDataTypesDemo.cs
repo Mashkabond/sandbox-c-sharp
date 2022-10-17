8907591f1e627306d3e92a751e77bedb71f431a4
alpa0298 <alpa0298@pm.me>
2022-10-17 20:45:39 +0300

    [main] - removed unnecessary files

--- starter_pack/console_calculator/Trainings/1_VariablesAndDataTypes/demo/VariablesAndDataTypesDemo.cs
+++ starter_pack/console_calculator/Trainings/1_VariablesAndDataTypes/demo/VariablesAndDataTypesDemo.cs
@@ -4,71 +4,59 @@ namespace variables_and_data_types
 {
     internal class VariablesAndDataTypesDemo
     {
-        //static void Main(string[] args)
-        //{
-        //    Cat cat1 = new Cat();
-        //    cat1.Name = "Я Русский";
+        static void Main(string[] args)
+        {
+            // declaration - <type> <name>;
+            int x;
+            int y;
 
-        //    Cat cat2 = new Cat();
-        //    cat2.Name = "Мурзик";
+            // assignment
+            x = 5;
+            y = 10;
 
-        //    Cat cat3 = new Cat();
-        //    cat3.Name = "Котик";
-        //    cat3.ChangeName("Мурзик2");
+            // declaration and assignment
+            int a = 10;
+            int b = 20;
 
+            // different types
+            string str = "some text";
+            char c = 'a';
+            double d = 12.34;
+            float f = 1.0f;
+            bool check = true; // true/false
 
-        //    // declaration - <type> <name>;
-        //    //int x;
-        //    //int y;
+            // math operations
+            a = b;          // a = 20
 
-        //    //// assignment
-        //    //x = 5;
-        //    //y = 10;
+            b = b + 1;      // b = 21
+            b = b - 1;      // b = 20
+            b -= 1;         // b = 19
+            b--;            // b = 18
+            --b;            // b = 17
 
-        //    //// declaration and assignment
-        //    //int a = 10;
-        //    //int b = 20;
+            a = a * 2;      // a = 40
+            a = a / 2;      // a = 20
+            a = b % 2;      // a = 1
+            a++;            // a = 2
+            ++a;            // a = 3
+            a = b++;        // a = 17
+            a = ++b;        // a = 19
+            a = a + 2 * b;  // a = 57
 
-        //    //// different types
-        //    //string str = "some text";
-        //    //char c = 'a';
-        //    //double d = 12.34;
-        //    //float f = 1.0f;
-        //    //bool check = true; // true/false
+            // Write to console
+            Console.WriteLine(a);
+            Console.WriteLine("some text");
 
-        //    //// math operations
-        //    //a = b;          // a = 20
+            // Read from console
+            string input = Console.ReadLine();
 
-        //    //b = b + 1;      // b = 21
-        //    //b = b - 1;      // b = 20
-        //    //b -= 1;         // b = 19
-        //    //b--;            // b = 18
-        //    //--b;            // b = 17
+            // Parse string to different types
+            int intParsed = int.Parse(input);
+            double doubleParsed = double.Parse(input);
+            double floatParsed = float.Parse(input);
 
-        //    //a = a * 2;      // a = 40
-        //    //a = a / 2;      // a = 20
-        //    //a = b % 2;      // a = 1
-        //    //a++;            // a = 2
-        //    //++a;            // a = 3
-        //    //a = b++;        // a = 18
-        //    //a = ++b;        // a = 19
-        //    //a = a + 2 * b;  // a = 57
-
-
-        //    // Write to console
-        //    //Console.WriteLine(a);
-        //    //Console.WriteLine("some text");
-
-        //    // Read from console
-        //    //string input = Console.ReadLine();
-
-        //    // Parse string to different types
-        //    //int intParsed = int.Parse(input);
-        //    //double doubleParsed = double.Parse(input);
-        //    //double floatParsed = float.Parse(input);
-
-        //    // Convert int to double - manual conversion
-        //    //double converted = (double)a / b;
-        //}
+            // Convert int to double - manual conversion
+            double converted = (double)a / b;
+        }
     }
 }
